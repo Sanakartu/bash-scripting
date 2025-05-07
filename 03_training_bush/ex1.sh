a1ebef7899e2f1c5a86af62fa17facda7870c49f
sanatosha <sanakarpenok9@gmail.com>
2025-05-07 17:56:53 +0300

    I did modified my ex1.sh script.

--- 03_training_bush/ex1.sh
+++ 03_training_bush/ex1.sh
@@ -1,6 +1,12 @@
 
 #!/bin/bash
-echo "Привет, $USER! Сегодняшняя дата: $(date +%d.%m.%Y)"  
+echo "Привет, $USER! Сегодняшняя дата: $(date +%d.%m.%Y)"
+
+CURRENTDIR=$1
+echo "Going to the another folder: $CURRENTDIR"
+cd $CURRENTDIR
+echo "Рабочий каталог: $CURRENTDIR"
+
 echo "Список файлов в текущей директории:"                  
-ls -l                                                        
-echo "Рабочий каталог: $PWD"                                
+ls -l
+                                
