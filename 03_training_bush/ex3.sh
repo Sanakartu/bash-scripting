a1b9ec9b0cfb73817666f9804548d6760b02db47
sanatosha <sanakarpenok9@gmail.com>
2025-05-11 14:33:31 +0300

    English time!... again,,,

--- 03_training_bush/ex3.sh
+++ 03_training_bush/ex3.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-read -p "Введите путь к директории или команду: " CURRENTDIR
-echo "Привет, $USER! Сегодняшняя дата: $(date +%d.%m.%Y)"
+read -p "Lets pick a dir or command " CURRENTDIR
+echo "Ayo, $USER! Date : $(date +%d.%m.%Y)"
 
 #if CURRENTDIR == cd .. then 
 if [[ "$CURRENTDIR" == "cd .." ]] then
     echo "Вы ввели команду, выполняю..."
     cd ..  # Doing like command
-    echo "Переходим в директорию: $CURRENTDIR"
-    echo "Рабочий каталог: $(pwd)"
-    echo "Список файлов в текущей директории:"
+    echo "Lets pick a dir: $CURRENTDIR"
+    echo "Going to: $(pwd)"
+    echo "Your ls -l:"
     ls -l
 else
-    echo "Вы ввели путь к директории, выполняю..."
+    echo "You wanted a path, executing..."
     cd "$CURRENTDIR"  # Doing like path
-    echo "Переходим в директорию: $CURRENTDIR"
-    echo "Рабочий каталог: $(pwd)"
-    echo "Список файлов в текущей директории:"
+    echo "Switching dir: $CURRENTDIR"
+    echo "Working dir: $(pwd)"
+    echo "Your ls -l ... ... again:"
     ls -l
 
 
